eea44a27dcf3b824f4aa68298885aded6ca32609
jugalw13 <33573362+jugalw13@users.noreply.github.com>
2017-11-11 22:52:12 +0530

    Update mult.cpp

--- mult.cpp
+++ mult.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <math.h>
+#include <cmath>
 using namespace std;
-vector<std::vector<int> > multiply(vector<vector<int> >a,int b[2][2],int n,int m);
-vector<std::vector<int> >z;
-struct node 
-{
-int mat[2][2];
-int siz;
-node *next;
-}*start,*newptr,*save,*ptr,*rear;
+int Q[64][64];
+vector<vector<int>>P;
+vector<vector<int>>N;
+vector<vector<int>>H;
+vector<vector<int>> Tensor_Product(vector<vector<int> >a,int b[2][2],int n,int m);
 
-node *create(int n)
+struct node
 {
-	ptr=new node;
-	ptr->siz=n;
-	ptr->mat[0][0]=1;
+    int mat[2][2];
+    int size;
+    node *next;
+}*polyzgate,*notgate,*hadamardgate,*newptr,*ptr,*rearpolyz,*rearnot,*rearhardamard;
+
+node *createpoly(int n)
+{
+    ptr=new node;
+    ptr->size=n;
+    ptr->mat[0][0]=1;
     ptr->mat[0][1]=0;
     ptr->mat[1][0]=0;
     ptr->mat[1][1]=-1;
@@ -23,117 +27,242 @@ node *create(int n)
     return ptr;
 }
 
-void insert(node *np)
+node *createnot(int n)
 {
-	if(start==NULL)
-		start=rear=np;
-	else
-	{
-		rear->next=np;
-		rear=np;
-	}
+    ptr=new node;
+    ptr->size=n;
+    ptr->mat[0][0]=0;
+    ptr->mat[0][1]=1;
+    ptr->mat[1][0]=1;
+    ptr->mat[1][1]=0;
+    ptr->next=NULL;
+    return ptr;
 }
 
-void pauli(int n)
+node *createhadamard(int n)
 {
-int i,j,m=1;
-for(i=0;i<n-1;i++)
-{   
-	m=m*2;
-	newptr=create(m);
-	insert(newptr);
+    ptr=new node;
+    ptr->size=n;
+    ptr->mat[0][0]=1;
+    ptr->mat[0][1]=1;
+    ptr->mat[1][0]=1;
+    ptr->mat[1][1]=-1;
+    ptr->next=NULL;
+    return ptr;
 }
-while(start!=NULL)
-{   
-	cout<<"\n\n\n";
-	for(i=0;i<2;i++)
+
+void insertpolyz(node *np)
+{
+    if(polyzgate==NULL)
+        polyzgate=rearpolyz=np;
+    else
     {
-	for(j=0;j<2;j++)
-	{
-		cout<<start->mat[i][j]<<"      ";
-	}
-	cout<<"\n";
-   }
-	z=multiply(z,start->mat,start->siz,2);
-	start=start->next;
-};
-}
-
-
-
-
-
-
-
-
-
-
-
-vector<std::vector<int> > multiply(vector<vector<int> >a,int b[2][2],int n,int m)
-{
-/*int**c = new int*[n*m];
-for(int p=0;p<n*m;++p)
-    c[p]=new int[n*m];*/
-vector<std::vector<int> >c;
-c.resize(n*m, std::vector<int>(n*m,0));
-int i,j,l,k;
-for(i=0;i<n;i++)
-{
-	for(j=0;j<n;j++)
-	{
-		for(l=0;l<m;l++)
-		{
-			for(k=0;k<m;k++)
-			{
-				c[i*m+l][j*m+k]=a[i][j]*b[l][k];
-			}
-		}
-	}
-}
-return c;
-}
-
-
-
-
-
-
-int main()
-{   start=rear=NULL;
-	int b[2][2];
-	int n,m,i,j,ele;
-	cin>>n;
-	int k=pow(2,n);
-	
-    z.resize(k, std::vector<int>(k,0));
-	z[0][0]=1;
-    z[0][1]=0;
-    z[1][0]=0;
-    z[1][1]=-1;
-    b[0][0]=1;
-    b[0][1]=0;
-    b[1][0]=0;
-    b[1][1]=-1;
-    m=1;
-    pauli(n);
-    /*for(i=1;i<n;i++)
-    {	
-    m=m*2;
-	z=multiply(z,b,m,2);
+        rearpolyz->next=np;
+        rearpolyz=np;
     }
-	//m=m*2;
-    //a=multiply(a,b,m,2);
-    */
+}
+
+void insertnot(node *np)
+{
+    if(notgate==NULL)
+        notgate=rearnot=np;
+    else
+    {
+        rearnot->next=np;
+        rearnot=np;
+    }
+}
+
+void inserthadamard(node *np)
+{
+    if(hadamardgate==NULL)
+        hadamardgate=rearpolyz=np;
+    else
+    {
+        rearpolyz->next=np;
+        rearpolyz=np;
+    }
+}
+
+void polyz(int n)
+{
+    int i,j,m=1;
+    for(i=0;i<n-1;i++)
+    {
+        m=m*2;
+        newptr=createpoly(m);
+        insertpolyz(newptr);
+    }
+    while(polyzgate!=NULL)
+    {
+        /*cout<<"\n\n\n";
+        for(i=0;i<2;i++)
+        {
+            for(j=0;j<2;j++)
+            {
+                cout<<polyzgate->mat[i][j]<<"      ";
+            }
+            cout<<"\n";
+        }*/
+        P=Tensor_Product(P,polyzgate->mat,polyzgate->size,2);
+        polyzgate=polyzgate->next;
+    }
+}
+
+void qnot(int n)
+{
+    int i,j,m=1;
+    for(i=0;i<n-1;i++)
+    {
+        m=m*2;
+        newptr=createnot(m);
+        insertnot(newptr);
+    }
+    while(notgate!=NULL)
+    {
+        /*cout<<"\n\n\n";
+        for(i=0;i<2;i++)
+        {
+            for(j=0;j<2;j++)
+            {
+                cout<<notgate->mat[i][j]<<"      ";
+            }
+            cout<<"\n";
+        }*/
+        N=Tensor_Product(N,notgate->mat,notgate->size,2);
+        notgate=notgate->next;
+    }
+}
+
+void hadamard(int n)
+{
+    int i,j,m=1;
+    for(i=0;i<n-1;i++)
+    {
+        m=m*2;
+        newptr=createhadamard(m);
+        inserthadamard(newptr);
+    }
+    while(hadamardgate!=NULL)
+    {
+        /*cout<<"\n\n\n";
+        for(i=0;i<2;i++)
+        {
+            for(j=0;j<2;j++)
+            {
+                cout<<hadamardgate->mat[i][j]<<"      ";
+            }
+            cout<<"\n";
+        }*/
+        H=Tensor_Product(H,hadamardgate->mat,hadamardgate->size,2);
+        hadamardgate=hadamardgate->next;
+    }
+}
+
+vector<vector<int>> Tensor_Product(vector<vector<int> >a,int b[2][2],int n,int m)
+{
+    vector<vector<int>>c;
+    c.resize(n*m,vector<int>(n*m,0));
+    int i,j,l,k;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            for(l=0;l<m;l++)
+            {
+                for(k=0;k<m;k++)
+                {
+                    c[i*m+l][j*m+k]=a[i][j]*b[l][k];
+                }
+            }
+        }
+    }
+    return c;
+}
+
+void input()
+{
+    int n,m,i,j;
+    cout<<"enter the number of qubits"<<endl;
+    cin>>n;
+    int k=pow(2,n);
     
-	cout<<"\nOutput\n";
     for(i=0;i<k;i++)
     {
-	for(j=0;j<k;j++)
-	{
-		cout<<z[i][j]<<"      ";
-	}
-	cout<<"\n\n";
+        for(j=0;j<k;j++)
+        {
+            if(i==j)
+            {
+                Q[i][j]=1;
+            }
+            else
+            {
+                Q[i][j]=0;
+            }
+        }
+    }
+    /*for (i=0; i<k; i++) {
+        for (j=0; j<k; j++) {
+            cout<<Q[i][j]<<" ";
+        }
+        cout<<endl;
+    }*/
+    
+    polyzgate=rearpolyz=NULL;
+    P.resize(k,vector<int>(k,0));
+    P[0][0]=1;
+    P[0][1]=0;
+    P[1][0]=0;
+    P[1][1]=-1;
+    polyz(n);
+    /*cout<<"\nOutput\n";
+    for(i=0;i<k;i++)
+    {
+        for(j=0;j<k;j++)
+        {
+            cout<<P[i][j]<<"      ";
+        }
+        cout<<"\n\n";
+    }*/
+    
+    notgate=rearnot=NULL;
+    N.resize(k,vector<int>(k,0));
+    N[0][0]=0;
+    N[0][1]=1;
+    N[1][0]=1;
+    N[1][1]=0;
+    qnot(n);
+    /*cout<<"\nOutput\n";
+    for(i=0;i<k;i++)
+    {
+        for(j=0;j<k;j++)
+        {
+            cout<<N[i][j]<<"      ";
+        }
+        cout<<"\n\n";
+    }*/
+    
+    hadamardgate=rearhardamard=NULL;
+    H.resize(k,vector<int>(k,0));
+    H[0][0]=1;
+    H[0][1]=1;
+    H[1][0]=1;
+    H[1][1]=-1;
+    hadamard(n);
+    /*cout<<"\nOutput\n";
+    for(i=0;i<k;i++)
+    {
+        for(j=0;j<k;j++)
+        {
+            cout<<H[i][j]<<"      ";
+        }
+        cout<<"\n\n";
+    }*/
 }
 
-	return 0;
+int main()
+{
+    input();
+    
+    return 0;
 }
